e85063fb3cc04c9f9a64caf13835c044b7c45912
Hunter Jensen <hunter.jensen.cbc@gmail.com>
2023-09-27 07:52:41 -0600

    Initial Commit

---
+++ Text Validation App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Text Validation App
+//
+//  Created by Hunter Jensen on 9/27/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
